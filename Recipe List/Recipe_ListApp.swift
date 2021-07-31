c03c7be97ab87ece10659f09cf1f4fbdcade8519
agholson <87455899+agholson@users.noreply.github.com>
2021-07-31 11:01:31 -0400

    Initial Commit

---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by Shepherd on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
